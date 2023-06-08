3ef0398218ef44ae27f7f63c94cbe11882867844
jephthah40 <edemjephthah1@gmail.com>
2023-06-08 13:09:20 -0700

    My commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    length = len(argv) - 1
-    if length == 0:
-        print("{}".format("0 arguments."))
-    elif length == 1:
-        print("{}".format("1 argument:"))
-        print("1: {}".format(argv[1]))
-    else:
-        print("{:d} {}".format(length, "arguments:"))
-        for i in range(1, length + 1):
-            print("{:d}: {}".format(i, argv[i]))
+if __name__ != "__main__":
+    exit()
+
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
